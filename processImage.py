9ed3b7ac4525103146240149d738fd2e7aad2aab
Elaina Heraty <heratyelaina@gmail.com>
2019-07-29 22:14:54 -0400

    Update processImage.py

--- processImage.py
+++ processImage.py
@@ -1,5 +1,5 @@
 from IPython.display import Image
-Image(filename='Users/elainaheraty/pictures/1.PNG', width=300, height=200)
+Image(filename='Users/elainaheraty/pictures/1.PNG MobileNet-inference-image/1.PNG', width=300, height=200)
 
 preprocessed_image = prepare_image('1.PNG')
 predictions = mobile.predict(preprocessed_image)
@@ -7,5 +7,5 @@ predictions = mobile.predict(preprocessed_image)
 #imagenet utility function which returns top 5 imagenet class predictions + class ID, class label and probability
 results = imagenet_utils.decode_preidctions(predictions)
 results
-
+#
 #test on jupyter notebooks
